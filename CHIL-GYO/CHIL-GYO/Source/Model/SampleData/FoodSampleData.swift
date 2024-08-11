b29a01270e2094cb3eaf4f138157de3f175534cf
YuBin Kim <yubin412@naver.com>
2024-08-11 11:58:35 +0900

    [Feat] 이미지 데이터셋 추가

--- CHIL-GYO/CHIL-GYO/Source/Model/SampleData/FoodSampleData.swift
+++ CHIL-GYO/CHIL-GYO/Source/Model/SampleData/FoodSampleData.swift
@@ -49,31 +49,31 @@ extension Food {
                                   name: "A raw clam",
                                   notice: "Please be especially careful during the summer!",
                                   issues: [.foodPoisoning])
-    static let coffee = Food(foodImageData: UIImage(resource: .tunaSashimi).jpegData(compressionQuality: 1),
+    static let coffee = Food(foodImageData: UIImage(resource: ._11).jpegData(compressionQuality: 1),
                                   name: "Coffee",
                                   notice: "Caffeine is limited to 300mg per day",
                                   issues: [.complicationsOfChildbirth])
-    static let tea = Food(foodImageData: UIImage(resource: .tunaSashimi).jpegData(compressionQuality: 1),
+    static let tea = Food(foodImageData: UIImage(resource: ._12).jpegData(compressionQuality: 1),
                                   name: "Tea",
                                   notice: "Caffeine is limited to 300mg per day",
                                   issues: [.complicationsOfChildbirth])
-    static let soda = Food(foodImageData: UIImage(resource: .tunaSashimi).jpegData(compressionQuality: 1),
+    static let soda = Food(foodImageData: UIImage(resource: ._13).jpegData(compressionQuality: 1),
                                   name: "Soda",
                                   notice: "Caffeine is limited to 300mg per day",
                            issues: [.complicationsOfChildbirth, .gestationalDiabetes])
-    static let chocolate = Food(foodImageData: UIImage(resource: .tunaSashimi).jpegData(compressionQuality: 1),
+    static let chocolate = Food(foodImageData: UIImage(resource: ._14).jpegData(compressionQuality: 1),
                                   name: "Chocolate",
                                   notice: "Caffeine is limited to 300mg per day",
                                 issues: [.complicationsOfChildbirth, .gestationalDiabetes])
-    static let pizza = Food(foodImageData: UIImage(resource: .tunaSashimi).jpegData(compressionQuality: 1),
+    static let pizza = Food(foodImageData: UIImage(resource: ._15).jpegData(compressionQuality: 1),
                                   name: "Pizza",
                                   notice: "The possibility of atopic dermatitis cannot be ruled out.",
                                 issues: [.atopy])
-    static let hamburger = Food(foodImageData: UIImage(resource: .tunaSashimi).jpegData(compressionQuality: 1),
+    static let hamburger = Food(foodImageData: UIImage(resource: ._16).jpegData(compressionQuality: 1),
                                   name: "Hamburger",
                                   notice: "The possibility of atopic dermatitis cannot be ruled out.",
                                 issues: [.atopy])
-    static let frenchFries = Food(foodImageData: UIImage(resource: .tunaSashimi).jpegData(compressionQuality: 1),
+    static let frenchFries = Food(foodImageData: UIImage(resource: ._16).jpegData(compressionQuality: 1),
                                   name: "French fries",
                                   notice: "The possibility of atopic dermatitis cannot be ruled out.",
                                 issues: [.atopy])
