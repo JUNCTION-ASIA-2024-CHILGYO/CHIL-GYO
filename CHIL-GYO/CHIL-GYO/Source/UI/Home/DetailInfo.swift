083963fc71794c3ade5d3763b33627f89c3d6a75
LEMONY <jin.lee1028@gmail.com>
2024-08-11 10:06:36 +0900

    [Feat] MainView 구현하기 3

---
+++ CHIL-GYO/CHIL-GYO/Source/UI/Home/DetailInfo.swift
@@ -0,0 +1,18 @@
+//
+//  DetailInfo.swift
+//  CHIL-GYO
+//
+//  Created by JIN LEE on 8/11/24.
+//
+
+import SwiftUI
+
+struct DetailInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailInfo()
+}
